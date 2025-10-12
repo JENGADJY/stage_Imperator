05413c632bca22ca7bb29a06e90ecfa9a1c85f30
JENGADJY <JENGOERROR404@gmail.com>
2025-10-12 16:38:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Créez un fichier `.env` à la racine du projet avec le contenu suivant :
+
+```env
+
+MISTRAL_KEY=
+
+MISTRAL_AGENT=
+```
---
+++ Script_auto.py
@@ -0,0 +1,9 @@
+from mistralai import Mistral
+from dotenv import load_dotenv 
+import os
+import sys
+
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+load_dotenv()
